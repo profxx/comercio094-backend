9b4049b30328fa8dcbaf6ad484a33c3050a14619
Alexandre <acpimentel@gmail.com>
2024-05-13 13:32:12 -0300

    Criada a parte do Cliente (Entity, Service, Repository e Controller)

--- src/main/java/br/com/senai/comercio094/controller/ClienteController.java
+++ src/main/java/br/com/senai/comercio094/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,10 @@ public class ClienteController {
     public ResponseEntity<Cliente> update(@PathVariable Long id, @RequestBody Cliente cliente){
         Cliente clienteAtual = clienteService.update(id, cliente);
         return ResponseEntity.ok().body(clienteAtual);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteById(@PathVariable Long id){
+        return ResponseEntity.ok().body(clienteService.deleteById(id));
     }
 
 }
